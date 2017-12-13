a3c717194ccceede309b49191c5e73dc4d10c81a
13153 <13153@hakodate.kosen-ac.jp>
2017-12-13 10:21:45 +0900

    移動

--- SCR2-9/A.java
+++ SCR2-9/A.java
@@ -14,18 +14,16 @@ public class A extends Actor
      */
     public void act() 
     {
-<<<<<<< HEAD
-<<<<<<< HEAD
+
         if( Greenfoot.isKeyDown( "left" ) ){
             setRotation(180);
             move(1);
-=======
+        }
         if( Greenfoot.isKeyDown( "right" ) ){
         setRotation(0);
         move(1);
         }// Add your action code here.
->>>>>>> 06c17a3d43dbbd04b91be74d38d29b5a34208544
-=======
+
         if( Greenfoot.isKeyDown( "up" ) ){
         setRotation(270);
         move(1);
@@ -33,8 +31,8 @@ public class A extends Actor
         if( Greenfoot.isKeyDown( "down" ) ){
         setRotation(90);
         move(1);
->>>>>>> 8dbfd7ef9949b4de9e86968bae8a4a54cab60ffa
+
         }
->>>>>>> 8dbfd7ef9949b4de9e86968bae8a4a54cab60ffa
+
     }    
 }
