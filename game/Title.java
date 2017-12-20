aaaf3372c2c4146cfd6eb6519b320b7bab7f467c
13153 <13153@hakodate.kosen-ac.jp>
2017-12-20 10:31:10 +0900

    スプリント1終了仮ver2

--- game/Title.java
+++ game/Title.java
@@ -16,6 +16,13 @@ public class Title extends World
     public Title()
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
-        super(600, 400, 1); 
+        super(800, 600, 1);
+        
+    }
+    public void act(){
+           if(Greenfoot.isKeyDown("space")){
+               World w = new MyWorld();
+               Greenfoot.setWorld(w);
+           }
     }
 }
